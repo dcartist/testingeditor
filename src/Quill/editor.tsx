0f340996b0e10736b4b25f7ce2c1b49ce7b46c9f
dcartist <pbannerman13@gmail.com>
2022-12-30 13:15:10 -0500

    updated editor

--- src/Quill/editor.tsx
+++ src/Quill/editor.tsx
@@ -7,9 +7,26 @@ export default function Editor() {
     const [value, setValue] = useState('');
     console.log(value)
     console.log(typeof value)
+   const  modules = {
+        toolbar: [
+          [{ 'header': [1, 2, false] }],
+          ['bold', 'italic', 'underline','strike', 'blockquote'],
+          [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+          ['link', 'image'],
+          ['clean']
+        ],
+      }
+    
+      const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+      ]
   return (
     <div>
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
+        <ReactQuill theme="snow" value={value} onChange={setValue}  modules={modules} formats={formats} />
+        {/* <ReactQuill theme="snow" value={value} onChange={setValue} /> */}
     </div>
   )
 }
